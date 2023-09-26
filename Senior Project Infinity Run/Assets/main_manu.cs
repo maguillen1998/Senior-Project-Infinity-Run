476099ff833240e693ea14c67ac1a2b6a56fb0db
victorvega26 <139924893+victorvega26@users.noreply.github.com>
2023-09-25 20:29:29 -0500

    menus
    added a main menu and options menu

---
+++ Senior Project Infinity Run/Assets/main_manu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.SceneManager;
+
+
+public class main_manu : MonoBehaviour
+{
+   public void PlayGame()
+    {
+        
+      // SceneManager.loadScene(SceneManager.GetActiveSene().buildIndex + 1);
+    }
+}
