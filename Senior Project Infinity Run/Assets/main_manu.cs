871b8d83bf325da9ea0f5e1ef13bafba5027badc
maguillen1998 <32723197+maguillen1998@users.noreply.github.com>
2023-09-25 20:50:02 -0500

    Fixed MainMenu Script Error
    Fixed UnityEngine.SceneManagement not found

--- Senior Project Infinity Run/Assets/main_manu.cs
+++ Senior Project Infinity Run/Assets/main_manu.cs
@@ -1,14 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using UnityEngine.SceneManager;
 
 
 public class main_manu : MonoBehaviour
 {
    public void PlayGame()
     {
-        
-      // SceneManager.loadScene(SceneManager.GetActiveSene().buildIndex + 1);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
