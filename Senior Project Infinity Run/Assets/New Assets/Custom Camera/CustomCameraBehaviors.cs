8fc886ee25ae44873de9e24f4a80c5db7b8e7e83
maguillen1998 <32723197+maguillen1998@users.noreply.github.com>
2023-09-30 11:21:29 -0500

    Updated Custom Camera
    1) added functionality to Custom Camera script to allow the application of a dynamic camera offset relative to the player. this allows us to dynamically alter the offset position of the camera during runtime
    1.1) I created a prefab specifically for FollowPlayerCamera

--- Senior Project Infinity Run/Assets/New Assets/Custom Camera/CustomCameraBehaviors.cs
+++ Senior Project Infinity Run/Assets/New Assets/Custom Camera/CustomCameraBehaviors.cs
@@ -6,17 +6,19 @@ public class CustomCameraBehaviors : MonoBehaviour
 {
     //set to null to allow checking if the player has not been found. 
     GameObject player = null;
+    
 
     //Important to offset on the Z axis to avoid accidentally clipping the PC, causing their sprite to disappear due to it being too close to the camera
     //z offset must be > the  close clipping plane of the camera
     Vector3 offset;
-    float ZOffset = -10f;
+    public float XOffset = 0f;
+    public float YOffset = 0f;
+    public float ZOffset = -10f;
     // Start is called before the first frame update
     void Start()
     {
-        float camHeight = this.GetComponent<Camera>().orthographicSize * 2f;
-        float Yoffset = camHeight/4f;
-        offset = new Vector3(0, Yoffset, ZOffset);
+        //float camHeight = this.GetComponent<Camera>().orthographicSize * 2f;
+        UpdateCameraOffset();
         //if checks to make sure that the "Player Character" tag exists before trying to access it to assign to the player variable
         if (GameObject.FindWithTag("Player Character"))
         {
@@ -35,12 +37,23 @@ public class CustomCameraBehaviors : MonoBehaviour
         if (player != null)
         {
             //set the camera position to the player position offset by the offset value
-            transform.position = player.transform.position + offset;
+            UpdateCameraOffset();
+            ApplyPlayerTransformToCameraWithOffset();
         }
         else
         {
             Debug.Log("Camera: Player not found, cannot update camera position");
             //may be viable to attempt to find GameObject with "player Charcter" tag
         }
+    }
+
+    void UpdateCameraOffset()
+    {
+        offset = new Vector3(XOffset, YOffset, ZOffset);
+    }
+
+    void ApplyPlayerTransformToCameraWithOffset()
+    {
+        transform.position = player.transform.position + offset;
     }
 }
